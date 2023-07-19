f58910a06ac63052d8cbd23ce42a25229453cbc7
Ivan Beshkiltsev <ivanbeshka2002@gmail.com>
2023-07-19 17:08:49 +0500

    remove port form allowHost

--- src/main/kotlin/ru/kaleidoscope/plugins/CORS.kt
+++ src/main/kotlin/ru/kaleidoscope/plugins/CORS.kt
@@ -7,7 +7,7 @@ import io.ktor.server.plugins.cors.routing.*
 fun Application.configureCORS() {
     install(CORS){
         maxAgeInSeconds = 600 //sec
-        allowHost( "string-art.vercel.app:3000", schemes = listOf("https"))
+        allowHost( "string-art.vercel.app", schemes = listOf("https"))
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Options)
